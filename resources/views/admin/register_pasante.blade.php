edac968fb6f585bc2fd2f2556744f4b5f2fc4055
Hamilton Ortiz Rodriguez <ortizrodriguezhaminton18@gmail.com>
2025-03-17 13:54:26 -0500

    Formulario creacion de usuario Pasante

---
+++ resources/views/admin/register_pasante.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.masteradmin')
+
+@section('content')<br>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header text-center">Registrar Pasante</div>
+
+                <div class="card-body">
+                    <form method="POST" action="">
+                        @csrf
+
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Nombre</label>
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="new-name" required autofocus>
+                            @error('name')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Correo Electrónico</label>
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="new-email" required>
+                            @error('email')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Contraseña</label>
+                            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password" required>
+                            @error('password')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="password_confirmation" class="form-label">Confirmar Contraseña</label>
+                            <input type="password" class="form-control" name="password_confirmation" required autocomplete="new-password" required>
+                        </div>
+
+                        <input type="hidden" name="role" value="mayordomo">
+
+                        <div class="d-grid">
+                            <button type="submit" class="btn btn-success">Registrar Mayordomo</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
